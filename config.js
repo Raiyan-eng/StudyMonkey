dea76fcc45bce4e24bc7141ce24d937877519335
Raiyan-eng <raijustexists@gmail.com>
2026-09-05 22:09:27 +0600

    Fix Firebase login configuration

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyDLc3Y8CDCSCxlMUchQxjWLKymwga-E6us',
+  apiKey: 'AIzaSyDLc3Y8CDCSCxlMUcHoXiWlKymwqa-E6us',
   authDomain: 'studymonkey-4be3e.firebaseapp.com',
   projectId: 'studymonkey-4be3e',
   storageBucket: 'studymonkey-4be3e.firebasestorage.app',
@@ -7,3 +7,4 @@ export const FIREBASE_CONFIG = {
   appId: '1:190856108981:web:6c95fdf64f52665ad857fc',
   measurementId: 'G-4LT4MPFR3H'
 };
+
